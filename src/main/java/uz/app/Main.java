23645d4ef28e79bbcd6f3abe693ecb6fa4e6cc83
sardor-kushakov <sardorkushakov.uz@gmail.com>
2024-07-06 21:48:12 +0500

    first day 2

--- src/main/java/uz/app/Main.java
+++ src/main/java/uz/app/Main.java
@@ -1,9 +1,28 @@
 package uz.app;
 
-import uz.app.entity.User;
+import java.util.Scanner;
 
 public class Main {
+
+    public static Scanner scanNum = new Scanner(System.in);
+    public static Scanner scanStr = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while (true) {
+            System.out.println("1. Sign Up  2. Sign In  0. Exit");
+            String command = scanStr.nextLine();
+
+            switch (command) {
+                case "1" -> signUp();
+                case "2" -> signIn();
+                case "0" -> {
+                    System.out.println("Bye, bye!");
+                    return;
+                }
+                default -> {
+                    System.out.println("Command not found, please try again!");
+                }
+            }
+        }
     }
 }
